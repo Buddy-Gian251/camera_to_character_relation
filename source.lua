74c1cddd00b973444a01778464aca5604464a431
Buddy-Gian251 <gian.bernaldez09@gmail.com>
2025-10-31 17:01:28 +0800

    Update source.lua

--- source.lua
+++ source.lua
@@ -12,15 +12,29 @@ local info_tween = TweenInfo.new(1, Enum.EasingStyle.Quad, Enum.EasingDirection.
 local SELF = Players.LocalPlayer
 local cam = workspace.CurrentCamera
 
+local PARENT
+do
+	local success, result = pcall(function()
+		if gethui then
+			return gethui()
+		elseif CoreGui:FindFirstChild("RobloxGui") then
+			return CoreGui
+		else
+			return SELF:WaitForChild("PlayerGui")
+		end
+	end)
+	PARENT = (success and result) or SELF:WaitForChild("PlayerGui")
+end
+print("PARENT: "..PARENT:GetFullName())
+task.wait()
 local gui = Instance.new("ScreenGui")
+gui.Name = "CFrameLockUI"
 gui.ResetOnSpawn = false
 gui.IgnoreGuiInset = true
-local success = pcall(function()
-	gui.Parent = CoreGui
-end)
-if not success then
-	gui.Parent = SELF:WaitForChild("PlayerGui")
-end
+gui.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
+gui.DisplayOrder = 9999
+gui.Enabled = true
+gui.Parent = PARENT
 
 local function makeDraggable(UIItem)
 	local dragging = false
@@ -54,61 +68,14 @@ local function makeDraggable(UIItem)
 end
 
 local button = Instance.new("TextButton")
---local config_frame = Instance.new("Frame")
---local config_layout = Instance.new("UIListLayout")
---local config_toggle = Instance.new("TextButton")
 button.Parent = gui
 button.Size = UDim2.new(0, 100, 0, 50)
-button.Position = UDim2.new(0.5, -50, 0.5, 250)
+button.Position = UDim2.new(0.5, -50, 0.5, 20)
 button.Text = "CFRAME LOCK"
 button.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
 button.TextColor3 = Color3.fromRGB(0, 0, 0)
 button.BorderSizePixel = 0
 button.Font = Enum.Font.GothamBold
---config_frame.Parent = gui
---config_frame.Size = UDim2.new(0, 250, 0, 250)
---config_frame.AnchorPoint = Vector2.new(0.5, 0.5)
---config_frame.Position = UDim2.new(0.5, 0, 0.75, 0)
---config_frame.Visible = false
---config_layout.Parent = config_frame
---config_layout.Padding = UDim.new(0, 10)
---config_layout.FillDirection = Enum.FillDirection.Vertical
---config_layout.HorizontalAlignment = Enum.HorizontalAlignment.Center
---config_layout.VerticalAlignment = Enum.VerticalAlignment.Top
---config_layout.SortOrder = Enum.SortOrder.LayoutOrder
---config_toggle.Visible = true
---config_toggle.Parent = config_frame
---config_toggle.AnchorPoint = Vector2.new(0, 1)
---config_toggle.Text = "Configuration"
-
---local function add_configuration(name, variable, callback)
---	local configcontain = Instance.new("Frame")
---	local textBox = Instance.new("TextBox")
---	local config_name = Instance.new("TextLabel")
---	configcontain.Parent = config_frame
---	configcontain.Size = UDim2.new(0, 200, 0, 50)
---	configcontain.Position = UDim2.new(0, 25, 0, 25)
---	configcontain.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
---	configcontain.BorderSizePixel = 0
---	configcontain.Visible = false
---	config_name.Parent = configcontain
---	config_name.Position = UDim2.new(0, 0, 0, 0)
---	config_name.Size = UDim2.new(0.5, 0, 1, 0)
---	config_name.Text = name
---	config_name.TextColor3 = Color3.fromRGB(0, 0, 0)
---	config_name.BackgroundTransparency = 1
---	config_name.BorderSizePixel = 0
---	textBox.Parent = configcontain
---	textBox.Size = UDim2.new(0.5, 0, 1, 0)
---	textBox.Position = UDim2.new(0, 1, 0, 0)
---	textBox.AnchorPoint = Vector2.new(1, 0)
---	textBox.BackgroundTransparency = 1
---	textBox.TextScaled = true
---	textBox.TextColor3 = Color3.fromRGB(0, 0, 0)
---	textBox.FocusLost:Connect(function()
---		callback(textBox.Text)
---	end)
---end
 
 makeDraggable(button)
 
@@ -125,9 +92,9 @@ local function toggle_loop_lock()
 			local HRP = character:FindFirstChild("HumanoidRootPart") :: BasePart
 			if not character or not HRP then return end
 			local cameraPivot = cam.CFrame
-			local look = cam.LookVector
-			local yaw = math.atan2(look.X, look.Z)
-			local newPivot = CFrame.new(HRP.Position) * CFrame.Angles(0, yaw, 0)
+			local look = cameraPivot.LookVector
+			local x, y, z = cameraPivot:ToOrientation()
+			local newPivot = CFrame.new(HRP.Position) * CFrame.Angles(0, y, 0)
 			HRP:PivotTo(newPivot)
 		end)
 	end
